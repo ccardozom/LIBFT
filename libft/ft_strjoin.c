9b49fca4dee6ac08f1c5b797143dd409682fee98
Carlos alberto Cardozo medina <ccardozo@c1r16s2.42madrid.com>
2020-02-04 12:31:20 +0100

    mejora en ft_strjoin

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,53 +6,29 @@
 /*   By: ccardozo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/14 18:15:22 by ccardozo          #+#    #+#             */
-/*   Updated: 2019/11/14 18:33:18 by ccardozo         ###   ########.fr       */
+/*   Updated: 2020/02/04 12:30:11 by ccardozo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static char	*ft_strcat(char *dest, const char *src)
-{
-	int	i;
-	int	j;
+#include "libft.h"
 
-	i = 0;
-	while (dest[i])
-		i++;
-	j = 0;
-	while (src[j])
-	{
-		dest[i + j] = src[j];
-		j++;
-	}
-	dest[i + j] = '\0';
-	return (dest);
-}
-
-static char	*ft_strcpy(char *dest, const char *src)
-{
-	int			i;
-
-	i = 0;
-	while (src[i])
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
-}
-
-char		*ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*joined;
 
-	if (s1 == NULL || s2 == NULL)
+	if (!s1 && !s2)
 		return (NULL);
-	if ((joined = (char*)malloc(sizeof(char)
-			* (ft_strlen(s1) + ft_strlen(s2) + 1))) == NULL)
-		return (NULL);
-	joined = ft_strcpy(joined, s1);
-	return (ft_strcat(joined, s2));
+	else if (s1 && !s2)
+		return (ft_strdup(s1));
+	else if (!s1 && s2)
+		return (ft_strdup(s2));
+	else
+	{
+		if (!(joined = malloc((ft_strlen(s1) + ft_strlen(s2)) + 1)))
+			return (NULL);
+		ft_strcat(ft_strcpy(joined, s1), s2);
+		return (joined);
+	}
 }
