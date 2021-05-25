4799c11c11df3089ad1f219fb7906cbe9b2b7765
ccardozom <ch4c0p4r4gu4y0@LAPTOP-6P4RP79M.localdomain>
2021-05-25 21:50:35 +0200

    add fix ft_memcmp.c to pass norminette v3.3.0

--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -3,19 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ccardozo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: ccardozo <ccardozo@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/06 14:44:52 by ccardozo          #+#    #+#             */
-/*   Updated: 2019/11/21 14:19:53 by ccardozo         ###   ########.fr       */
+/*   Updated: 2021/05/25 21:34:49 by ccardozo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int			ft_memcmp(const void *s1, const void *s2, size_t n)
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	unsigned const char		*t1;
-	unsigned const char		*t2;
+	unsigned const char	*t1;
+	unsigned const char	*t2;
 
 	t1 = (unsigned const char *)s1;
 	t2 = (unsigned const char *)s2;
