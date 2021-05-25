70a20a0c0c292fb795c2add24bfdabb266055fcf
ccardozom <ch4c0p4r4gu4y0@LAPTOP-6P4RP79M.localdomain>
2021-05-25 22:28:55 +0200

    add fix atoi.c to pass norminette v3.3.0

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ccardozo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: ccardozo <ccardozo@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/08 08:20:01 by ccardozo          #+#    #+#             */
-/*   Updated: 2019/11/21 10:21:41 by ccardozo         ###   ########.fr       */
+/*   Updated: 2021/05/25 22:28:35 by ccardozo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int				ft_atoi(const char *s)
+int	ft_atoi(const char *s)
 {
 	int			p;
 	long int	nb;
@@ -20,8 +20,15 @@ int				ft_atoi(const char *s)
 	nb = 0;
 	while (*s == 32 || (*s >= 9 && *s <= 13))
 		s++;
-	p = (*s == '-') ? -1 : 1;
-	(*s == '-' || *s == '+') ? s++ : s;
+	if (*s == '-')
+	{
+		p = -1;
+		s++;	
+	}
+	else
+		p = 1;
+	if (*s == '+')
+		s++;
 	while (ft_isdigit(*s))
 	{
 		nb = nb * 10 + (*s - '0');
