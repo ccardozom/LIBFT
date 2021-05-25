13f9f0351f3ec72fb25fc41e57eb460803469b49
ccardozom <ch4c0p4r4gu4y0@LAPTOP-6P4RP79M.localdomain>
2021-05-25 21:51:42 +0200

    add fix ft_memccpy.c to pass norminette v3.3.0

--- libft/ft_memccpy.c
+++ libft/ft_memccpy.c
@@ -3,29 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memccpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ccardozo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: ccardozo <ccardozo@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/04 16:01:42 by ccardozo          #+#    #+#             */
-/*   Updated: 2019/11/21 11:10:18 by ccardozo         ###   ########.fr       */
+/*   Updated: 2021/05/25 21:51:27 by ccardozo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void		*ft_memccpy(void *dst, const void *src, int c, size_t n)
+void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
 {
 	size_t			i;
 	unsigned char	*ptr_dst;
 	unsigned char	*ptr_src;
 
-	ptr_dst = (unsigned char*)dst;
-	ptr_src = (unsigned char*)src;
+	ptr_dst = (unsigned char *)dst;
+	ptr_src = (unsigned char *)src;
 	i = 0;
 	while (i < n)
 	{
 		ptr_dst[i] = ptr_src[i];
 		if (ptr_dst[i] == (unsigned char)c)
-			return ((void*)(dst + i + 1));
+			return ((void *)(dst + i + 1));
 		++i;
 	}
 	return (NULL);
