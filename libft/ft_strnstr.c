3b5923136ebd16e0952bf6b1558b1897528f0486
ccardozom <ch4c0p4r4gu4y0@LAPTOP-6P4RP79M.localdomain>
2021-05-25 09:17:35 +0200

    fix ft_strnstr.c to pass norminette V3.3.0

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -3,31 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ccardozo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: ccardozo <ccardozo@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/19 19:58:38 by ccardozo          #+#    #+#             */
-/*   Updated: 2019/11/19 17:34:46 by ccardozo         ###   ########.fr       */
+/*   Updated: 2021/05/25 09:16:31 by ccardozo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char		*ft_strnstr(const char *h, const char *n, size_t len)
+char	*ft_strnstr(const char *h, const char *n, size_t len)
 {
 	size_t	i;
 	size_t	j;
 
 	i = 0;
-	if (*n == '\0' || n == NULL)
-		return ((char*)h);
-	while (h[i] != '\0' && i < len)
+	if (!n)
+		return ((char *)h);
+	while (h[i] && i < len)
 	{
 		j = 0;
 		while (n[j] == h[i + j] && i + j < len)
 		{
 			if (n[j + 1] == '\0')
 			{
-				return ((char*)h + i);
+				return ((char *)h + i);
 			}
 			j++;
 		}
