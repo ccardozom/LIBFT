3a52cc264626de76c16b59d36d3e969a43660289
ccardozom <ch4c0p4r4gu4y0@LAPTOP-6P4RP79M.localdomain>
2021-05-27 17:21:05 +0200

    fix all funtions to pass norminette V3.3.0

--- libft-war-machine-v2019/my_config.sh
+++ libft-war-machine-v2019/my_config.sh
@@ -5,7 +5,7 @@
 ###################################
 
 #Edit the next line with the path of your libft project.
-PATH_LIBFT=/Users/ccardozo/Documents/42madrid/libft
+PATH_LIBFT=/home/ch4c0p4r4gu4y0/libft/libft
 
 #If you use a relative path, don't put double quotes.
 #For example :
